578c016145cf0e8eb901c267d5018ec9b0b7a3c9
Willem Jan Grootjans <willem.jan.grootjans@gmail.com>
2020-08-30 17:49:40 +0200

    Disable auto update

--- ledmatrix.py
+++ ledmatrix.py
@@ -8,23 +8,26 @@ class ledmatrix:
         self._height = height
         self._width = width
         self._total_length = height*width
-        self._neopixel =  neopixel.NeoPixel(board.D18, self._total_length, pixel_order=pixel_encoding)
+        self._neopixel =  neopixel.NeoPixel(board.D18, self._total_length, auto_write=False, pixel_order=pixel_encoding)
         self._neopixel[0] = (255,0,0)
         
 
     def set_pixel(self, x, y, color):
-        i = self.calculate_index(x, y)
-        
-        self._neopixel[i] = color[:3]
+        self.__set_pixel(x, y, color)
+        self._neopixel.show()
 
 
     def set_image(self, x, y, image):
-    
         size_x = min(image.width, self._width - x)
         size_y = min(image.height, self._height - y)
         for xx in range(0, size_x):
             for yy in range(0, size_y):
-                self.set_pixel(x+xx, y+yy, image.getpixel((xx, yy)))
+                self.set_pixel(x+xx, y+yy, image.getpixel((xx, yy))[:3])
+        self._neopixel.show()
+
+    def __set_pixel(self, x, y, color):
+        i = self.calculate_index(x, y)
+        self._neopixel[i] = color
 
     def calculate_index(self, x, y):
         row = self._height - y
